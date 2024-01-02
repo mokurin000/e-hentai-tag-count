0f52e9a0a373cbb9bf29b37b675b7a82095acdd6
poly000 <34085039+poly000@users.noreply.github.com>
2024-01-02 12:59:19 +0800

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -2,6 +2,8 @@ if ! [[ -z $1 ]]
 then exit 1
 fi
 
+output_file=${1%.sql.gz}.csv
+
 gzip -d -c "$1" |
   rg '^\(.*\)[,;]$' |
   tr -cd '[0-9] \n' |
@@ -10,4 +12,6 @@ gzip -d -c "$1" |
   sort -n -r |
   uniq -c |
   sed 's/^\s*//g;s/ /,/' |
-  sort -n -r > ${1%.sql.gz}.csv
+  sort -n -r > "${output_file}"
+
+gzip "${output_file}"
