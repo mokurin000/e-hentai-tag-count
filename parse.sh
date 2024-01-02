6ea74baeea79ab0c82d6f53aeb1cfc6062a843e7
poly000 <34085039+poly000@users.noreply.github.com>
2024-01-02 21:04:45 +0800

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-if [ -z $1 ]
+if [ $# != 2 ]
 then exit 1
 fi
 
 output_file=gid_tid.csv
 
 gzip -d -c "$1" |
-  rg '^\(.*\)[,;]$' |
+  grep -E '^\(.*\)[,;]$' |
   tr -cd '[0-9] \n' |
   grep -v '^$' |
   awk -F" " '{ print $2 }' |
@@ -17,4 +17,11 @@ gzip -d -c "$1" |
   sort -n -r |
   awk -F, '{ print $2 "," $1 }' > tid_count.csv
 
-gzip tid_count.csv
+gzip -d -c "$2" |
+  grep -E '^\(.*\)[,;]$' |
+  tr -d "(); " |
+  sed 's![,;]$!!' |
+  sort -n > tid_tag.csv
+
+gzip tid_count.csv &
+gzip tid_tag.csv &
