23699c88a7b064cda9333ca7e792eb68a6c09cd7
poly000 <34085039+poly000@users.noreply.github.com>
2024-01-02 22:33:58 +0800

    Update translation.py

--- utils/translation.py
+++ utils/translation.py
@@ -1,11 +1,22 @@
 import json
 
 with open("db.text.json", encoding="utf-8") as f:
-    tagdb = json.load(f)['data']
+    tagdb = json.load(f)["data"]
 
-tagdb = { item['namespace']: item['data'] for item in tagdb if not item['namespace'] in ['rows', 'reclass']}
+tagdb = {
+    item["namespace"]: item["data"]
+    for item in tagdb
+    if not item["namespace"] in ["rows", "reclass"]
+}
 
-for namespace, tags in tagdb.items():
-    for tag, info in tags.items():
-        full_tag = f"{namespace}:{tag}"
-        print(f"'{full_tag}'", f"'{info['name']}'", f"'{info['intro'].replace('\n', '\\n')}'", sep=',')
+with open("tag_name_intro.csv", encoding="utf-8", mode="w+") as o:
+    for namespace, tags in tagdb.items():
+        for tag, info in tags.items():
+            full_tag = f"{namespace}:{tag}"
+            print(
+                f"'{full_tag}'",
+                f"'{info['name']}'",
+                f"'{info['intro'].replace('\n', '\\n')}'",
+                sep=",",
+                file=o,
+            )
